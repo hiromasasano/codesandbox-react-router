db046ab3939a2b39e7c283f78cca3355f0a28925
HS <hsano@comsis.co.jp>
2023-06-05 00:00:22 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
@@ -5,6 +8,9 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <Home />
+      <Page1 />
+      <Page2 />
     </div>
   );
 }
