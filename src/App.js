19e97caed99300eadb50a4aa55cd830c95829be2
HS <hsano@comsis.co.jp>
2023-06-07 00:27:43 +0000

    Update App.js and Page1.jsx

--- src/App.js
+++ src/App.js
@@ -9,21 +9,27 @@ export default function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Link to="/">Home</Link>
+        <Link to={`/`}>Home</Link>
         <br />
-        <Link to="/page1">Page1</Link>
+        <Link to={`/page1`}>Page1</Link>
         <br />
-        <Link to="/page2">Page2</Link>
+        <Link to={`/page2`}>Page2</Link>
         <br />
       </div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path={`/`} element={<Home />} />
       </Routes>
       <Routes>
-        <Route path="/page1" element={<Page1 />} />
+        <Route path={`/page1`} element={<Page1 />} render={() => (
+          <Routes>
+            <Route path={`/`} element={<Page1 />} />
+            <Route path={`/page1/detailA`} element={<Page1DetailA />} />
+            <Route path={`/page1/detailB`} element={<Page1DetailB />} />
+          </Routes>
+        )}/>
       </Routes>
       <Routes>
-        <Route path="/page2" element={<Page2 />} />
+        <Route path={`/page2`} element={<Page2 />} />
       </Routes>
     </BrowserRouter>
   );
