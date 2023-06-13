4a17b4fe2386f4a88034228fdbee5efac1048c25
HS <hsano@comsis.co.jp>
2023-06-13 01:19:34 +0000

    Update 7 files

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Home } from "../Home";
-import { Page2 } from "../Page2";
 import { Page1Routes } from "./Page1Routes";
+import { Page2Routes } from "./Page2Routes";
+import { Page404 } from "../Page404";
 
 export const Router = () => {
   return (
@@ -11,7 +12,10 @@ export const Router = () => {
       {Page1Routes.map((route) => (
         <Route path={route.path} element={route.children} />
       ))}
-      <Route path={`page2`} element={<Page2 />} />
+      {Page2Routes.map((route) => (
+        <Route path={route.path} element={route.children} />
+      ))}
+      <Route path={`*`} element={<Page404 />} />
     </Routes>
   );
 };
